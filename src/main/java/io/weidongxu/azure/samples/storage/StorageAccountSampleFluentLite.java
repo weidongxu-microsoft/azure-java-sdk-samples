7310929c946b3e9bb5af1c165aa88fef2f355786
Weidong Xu <weidxu@microsoft.com>
2020-04-16 14:35:46 +0800

    lower java version to 9; add update to sample

--- src/main/java/io/weidongxu/azure/samples/storage/StorageAccountSampleFluentLite.java
+++ src/main/java/io/weidongxu/azure/samples/storage/StorageAccountSampleFluentLite.java
@@ -1,7 +1,9 @@
 package io.weidongxu.azure.samples.storage;
 
+import com.microsoft.azure.management.storage.v2019_06_01.AccessTier;
 import com.microsoft.azure.management.storage.v2019_06_01.Kind;
 import com.microsoft.azure.management.storage.v2019_06_01.SkuName;
+import com.microsoft.azure.management.storage.v2019_06_01.StorageAccount;
 import com.microsoft.azure.management.storage.v2019_06_01.implementation.SkuInner;
 import com.microsoft.azure.management.storage.v2019_06_01.implementation.StorageManager;
 import io.weidongxu.azure.samples.ResourceContext;
@@ -17,6 +19,14 @@ public class StorageAccountSampleFluentLite {
                         .withName(SkuName.STANDARD_LRS))
                 .withIsHnsEnabled(true)
                 .withEnableHttpsTrafficOnly(true)
+                .withTag("product", "javasdk")
+                .withTag("cause", "automation")
                 .create();
+
+        StorageAccount storageAccount = client.storageAccounts().getByResourceGroup(context.resourceGroup(), "sasamplefluentlite");
+        storageAccount.update()
+                .withoutTag("cause")    // bug, no effect
+                .withAccessTier(AccessTier.COOL)
+                .apply();
     }
 }
--- src/main/java/io/weidongxu/azure/samples/storage/StorageAccountSampleVanilla.java
+++ src/main/java/io/weidongxu/azure/samples/storage/StorageAccountSampleVanilla.java
@@ -1,11 +1,15 @@
 package io.weidongxu.azure.samples.storage;
 
+import com.azure.management.vanilla.storage.AccessTier;
 import com.azure.management.vanilla.storage.Kind;
 import com.azure.management.vanilla.storage.Sku;
 import com.azure.management.vanilla.storage.SkuName;
 import com.azure.management.vanilla.storage.StorageAccountCreateParameters;
+import com.azure.management.vanilla.storage.StorageAccountUpdateParameters;
 import com.azure.management.vanilla.storage.models.StorageManagementClientImpl;
 import io.weidongxu.azure.samples.ResourceContext;
+
+import java.util.Map;
 
 public class StorageAccountSampleVanilla {
 
@@ -17,6 +21,16 @@ public class StorageAccountSampleVanilla {
                         .setSku(new Sku()
                                 .setName(SkuName.STANDARD_LRS))
                         .setIsHnsEnabled(true)
-                        .setEnableHttpsTrafficOnly(true));
+                        .setEnableHttpsTrafficOnly(true)
+                        .setTags(Map.of("product", "javasdk",
+                                "cause", "automation")));
+
+        /* vanilla can skip this, on update if id or name is known and no info required from the resource.
+        StorageAccountInner storageAccount = client.storageAccounts().getByResourceGroup(context.resourceGroup(), "sasamplevanilla");
+         */
+        client.storageAccounts().update(context.resourceGroup(), "sasamplevanilla",
+                new StorageAccountUpdateParameters()
+                        .setTags(Map.of("product", "javasdk"))
+                        .setAccessTier(AccessTier.COOL));
     }
 }
